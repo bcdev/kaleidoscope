b275ec93190dc33d76774953314db4e4da4c3c85
Ralf Quast <ralf.quast@brockmann-consult.de>
2025-04-17 17:54:54 +0200

    Update: rename

--- test/kaleidoscope/test_generators.py
+++ test/kaleidoscope/test_generators.py
@@ -13,13 +13,13 @@ from kaleidoscope.generators import DefaultGenerator
 class DefaultGeneratorTest(unittest.TestCase):
 
     def test_random(self):
-        rng = DefaultGenerator(42)
-        self.assertEqual(6164909031098000398, rng.next_int64())
-        self.assertEqual(62765134502071353, rng.next_int64())
-        self.assertEqual(6068961337446000720, rng.next_int64())
-        self.assertEqual(3424215743300924766, rng.next_int64())
-        self.assertEqual(1906168894638979906, rng.next_int64())
-        self.assertEqual(1787925334117997081, rng.next_int64())
+        g = DefaultGenerator(42)
+        self.assertEqual(6164909031098000398, g.next_int64())
+        self.assertEqual(62765134502071353, g.next_int64())
+        self.assertEqual(6068961337446000720, g.next_int64())
+        self.assertEqual(3424215743300924766, g.next_int64())
+        self.assertEqual(1906168894638979906, g.next_int64())
+        self.assertEqual(1787925334117997081, g.next_int64())
 
 
 if __name__ == "__main__":
