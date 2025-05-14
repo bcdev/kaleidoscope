99bc5d0fb2ce16f11249f5e42574b4d58d196aa6
Ralf Quast <ralf.quast@brockmann-consult.de>
2025-05-14 14:23:01 +0200

    Update: decile normal generator

--- kaleidoscope/generators.py
+++ kaleidoscope/generators.py
@@ -4,8 +4,12 @@
 """
 This module provides random number generators.
 """
+from typing import Any
+from typing import Any
+from typing import Literal
 
 import numpy as np
+from numpy import ndarray
 from numpy.random import BitGenerator
 from numpy.random import Generator
 
@@ -82,7 +86,7 @@ class DefaultUniform(Uniform):
 
 
 class DefaultNormal(Normal):
-    """The default normal random variate."""
+    """The default normal random deviate."""
 
     _g: Generator
 
@@ -100,3 +104,65 @@ class DefaultNormal(Normal):
     def randoms(self, randoms: np.ndarray) -> np.ndarray:
         self._g.standard_normal(dtype=randoms.dtype, out=randoms)
         return randoms
+
+
+class DecileNormal(Normal):
+    """
+    The decile normal random deviate.
+
+    Generates random deviates, which are uniformly distributed in a
+    selected decile of the standard normal distribution.
+    """
+
+    _g: Generator
+
+    _q: ndarray[float, float]
+    """The deciles of the normal distribution."""
+    _s: int | Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """The decile selector."""
+
+    def __init__(
+        self, s: int, seed: int | np.ndarray | BitGenerator | None = None
+    ):
+        """
+        Creates a new random deviate.
+
+        :param s: The decile selector.
+        :param seed: The seed.
+        """
+        self._s = s % 10
+        self._g = default_generator(seed)
+        self._q = np.ndarray(
+            [
+                -3.09023,
+                -1.28155,
+                -0.841621,
+                -0.524401,
+                -0.253347,
+                0.0,
+                0.253347,
+                0.524401,
+                0.841621,
+                1.28155,
+                3.09023,
+            ]
+        )
+
+    def random(self) -> float:
+        return (self.sup - self.inf) * self._g.random() + self.inf
+
+    def randoms(self, randoms: np.ndarray) -> np.ndarray:
+        self._g.random(dtype=randoms.dtype, out=randoms)
+        randoms *= self.sup - self.inf
+        randoms += self.inf
+        return randoms
+
+    @property
+    def sup(self) -> float:
+        """Returns the supremum of the selected decile."""
+        return self._q[self._s + 1]
+
+    @property
+    def inf(self) -> float:
+        """Returns the infimum of the selected decile."""
+        return self._q[self._s]
