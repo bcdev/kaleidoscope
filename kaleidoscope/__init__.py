9c2b4423bba4022b8723b76e6a69a4d13a4777ed
Ralf Quast <ralf.quast@brockmann-consult.de>
2025-05-15 15:20:43 +0200

    Update: version number

--- kaleidoscope/__init__.py
+++ kaleidoscope/__init__.py
@@ -8,5 +8,5 @@ This Python package constitutes the Kaleidoscope processor.
 __name__ = "kaleidoscope"
 """The name of the Kaleidoscope processor."""
 
-__version__ = "2025.1.0"
+__version__ = "2025.1.1"
 """The version of the Kaleidoscope processor."""
